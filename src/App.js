f4f46bb7432b2f74d33ded80c67a3840fcb02235
Toha Islam <islamtoha710@gmail.com>
2022-09-28 23:27:15 +0600

    website left side completed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Exercises from './components/Exercises/Exercises';
 
 function App() {
   return (
-    <div>
+    <div className='full-layout'>
       <h1 className='icon text-4xl mt-2'><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Toha's-Fitness-Club</h1>
-      <h2 className='text-3xl mt-1'>Select Today's Exercise </h2>
+      <h2 className='text-3xl mt-2 mb-2'>Select Today's Exercise </h2>
       <Exercises></Exercises>
     </div>
   );
--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Exercise.css'
 
 const Exercise = (props) => {
-    const { img } = props.exercise;
+    const { img, CategoryName, age, time } = props.exercise;
+    console.log(props.exercise)
     return (
         <div className='exercise'>
             <img src={img} alt="" />
+            <div className="m-2">
+                <h2 className='text-1xl mt-0'>{CategoryName}</h2>
+                <p>For age: {age}</p>
+                <p>Time required:{time}</p>
+            </div>
+            <button className='bg-green-700 w-full btn h-10 text-white border-none'>Add to list</button>
         </div>
     );
 };
