3ffa4235687e46dced546850e1cd2a4d36dc1890
Toha Islam <islamtoha710@gmail.com>
2022-09-28 21:34:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import Exercises from './components/Exercises/Exercises';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='icon'><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Toha's-Fitness-Club</h1>
+      <h2>Select Today's Exercise </h2>
+      <Exercises></Exercises>
     </div>
   );
 }
---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import React from 'react';
+import './Exercises.css'
+
+const Exercises = () => {
+    return (
+        <div className='fitness-container'>
+
+            <div className="exercise-container">
+                <p>I am from exercise</p>
+            </div>
+            <div className="cart-container">
+                <p>I am from cart</p>
+            </div>
+        </div>
+    );
+};
+
+export default Exercises;
